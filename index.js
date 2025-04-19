03ce8a9703daf4cce3aaa08ffa4900ac036fb2c7
BorisovAleksey <aleksej.borisov.06@mail.ru>
2025-04-19 13:48:10 +0300

    -

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log(2) // Выведет 2 при загрузке страницы
+
+document.getElementById('myButton').addEventListener('click', function () {
+	console.log(3) // Выведет 3 при клике на кнопку
+})
